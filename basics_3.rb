76325456c1862ef8e48a367068e19b4a7f899484
Jana Götze <jana.goetze@gmail.com>
2018-09-10 20:13:19 +0200

    basics exercises

---
+++ basics_3.rb
@@ -0,0 +1,9 @@
+movies = {
+  "Titanic" => 1997,
+  "Eternal Sunshine of the Spotless Mind" => 2004,
+  "Hidden Figures" => 2016,
+  "Carnage" => 2011,
+  "Big Fish" => 2003
+}
+
+movies.each {|movie, year| puts year}
---
+++ basics_4.rb
@@ -0,0 +1,3 @@
+movie_year = [1997, 2004, 2016, 2011, 2003]
+
+movie_year.each {|year| puts year}
